74c1b605897a610f8e0cc5a66207b1bc6d444dde
Nikita <nikita_sokolov98@mail.ru>
2021-08-08 15:44:05 +0300

    changed public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '',
+    ? ''
+    : '/',
   assetsDir: 'assets',
   productionSourceMap: false,
   filenameHashing: true
